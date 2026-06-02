e90492ddc150cc4a41c1a7707aba737ed93d0ddc
Thomas Barkshire <thomasbarkshire@gmail.com>
2026-06-02 12:26:40 +0100

    created a review posting page

---
+++ frontend/Review/Review.js
@@ -0,0 +1,32 @@
+document.getElementById("review-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            movieName: form.get("movie-name"),
+            movieScore: form.get("movie-score"),
+            comments: form.get("comments")
+        })
+    }
+
+    const response = await fetch("http://localhost:3000/movie/", options);
+    const data = await response.json();
+    console.log(data);
+
+    if (response.status == 200) {
+        alert("Review posted")
+      } else {
+        alert(data.error);
+      }
+})
+
+document.getElementById("home-nav").addEventListener("click", () => {
+    window.location.assign("../Home/Home.html")
+})
