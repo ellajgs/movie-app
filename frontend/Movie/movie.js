02a871fcb49e42bbe5679b82a4c50dd84790ce92
Sajanth Kirubaharan <sajanth02@gmail.com>
2026-06-02 14:29:30 +0100

    added table row functionality

--- frontend/Movie/movie.js
+++ frontend/Movie/movie.js
@@ -1,19 +1,56 @@
 const movieHeader = document.querySelector("#movie-header")
-const movieTable = document.querySelector("#movie-table")
 const tableBody = document.querySelector("#movie-table-body")
 const backButton = document.querySelector("#back-button")
+const movieInfo = document.querySelector("#movie-info-container")
 
 backButton.addEventListener("click", () => {
-    window.location.assign("./Home/Home.html")
+    window.location.assign("../Home/Home.html")
 })
 
 function getMovieInfo(){}
+function getMovieReviews(){}
 
-const data;
+
 
 function generateTableRow(){
-    const tr = document.createElement("tr");
-    const td = document.createElement("td");
-    for(let i = 0; i++; i< data.length){}
+    
+        const data = [{
+            username: "test1",
+            rating: 3,
+            comment: " testcom1"},
+            
+            {username: "test2",
+            rating: 4,
+            comment: "testcom2"},
+            {username: "test3",
+            rating: 90,
+            comment: "testcomdfdfdf2"}
+        ]
+        
+   
+     for(let i = 0; i < data.length; i++){
+        const tr = document.createElement("tr");
+        const dataObject = data[i]
+        for(let key in dataObject){
+            const td = document.createElement("td");
+            td.innerHTML = dataObject[key]
+            tr.appendChild(td)
+        }
+        tableBody.appendChild(tr)
+    }
     
 }
+
+function displayMovieInfo(){
+    const img = document.createElement("img");
+    const text = document.createElement("p");
+
+
+    movieInfo.textContent
+    questionText.textContent = dialogue
+    gameContainer.appendChild(speechBubble)
+    speechBubble.appendChild(questionText)
+
+}
+
+generateTableRow()
