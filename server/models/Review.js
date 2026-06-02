ca44835f773d2021bcbfaaae1bead1824b44fed2
ella <ejgsimmons@outlook.com>
2026-06-02 14:55:08 +0100

    update review models

--- server/models/Review.js
+++ server/models/Review.js
@@ -2,6 +2,7 @@ const db = require(`../database/connect`)
 
 const Movie = require('./Movie')
 
+
 class Review{
     constructor({id, user_id, movie_id, user_rating, comments}){
         this.id=id
@@ -13,26 +14,19 @@ class Review{
 
     static async create(data){
         const {movieName, movieScore, comments, userID} = data;
-<<<<<<< HEAD
+        const movie = await Movie.findOrAdd(movieName)
 
-        const movie = await Movie.findOrAdd(movie)
+        const result = await db.query(
+            `INSERT INTO reviews (user_id, movie_id, user_rating, comments)
+             VALUES ($1, $2, $3, $4)
+             RETURNING *;`,
+            [userID, movie.id, movieScore, comments]
+        );
 
-        let response = await db.query("INSERT INTO reviews (user_rating, comments) VALUES ($1,$2) RETURNING *;" [movieScore, comments])
-=======
-        const url = `https://www.omdbapi.com/?t=${encodeURIComponent(movieName)}&apikey=${OMDB_API_KEY}`;
-        const response = await fetch(url);
-        const data2 = await response.json();
-
-        if (data2.Response === 'False') {
-            throw new Error(data.Error || 'Movie not found');
-        }
-
-        let response = await db.query("INSERT INTO reviews (user_id, user_rating, comments) VALUES (($1, $2, $3, $4) RETURNING *;" [movieScore, comments])
->>>>>>> 33e031d443f33b03ab0e0ae2214579fa74559945
-        return new Review(response.rows[0])
-    }
+        return new Review(result.rows[0]);
 
     
+}
 }
 
 module.exports = Review;
