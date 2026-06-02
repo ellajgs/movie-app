06a101883f7d971ca27d2eca75c4920385f204e2
Anthony-Omo <ajomotayo17@gmail.com>
2026-06-02 14:10:07 +0100

    db fix

--- server/database/setup.sql
+++ server/database/setup.sql
@@ -1,25 +1,29 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS reviews;
+
 --Users
-CREATE TABLE IF NOT EXISTS users (
-    id INT GENERATED ALWAYS AS IDENTITY 
-    username VARCHAR(50) UNIQUE NOT NULL
-    password_hash VARCHAR(255) NOT NULL
+CREATE TABLE users (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
 --Movies
-CREATE TABLE IF NOT EXISTS movies (
-    id INT GENERATED ALWAYS AS IDENTITY
-    title VARCHAR(100) UNIQUE 
-    imdb_rating INT
-    imdb_id VARCHAR
+CREATE TABLE movies (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR(100) UNIQUE,
+    imdb_rating INT,
+    imdb_id VARCHAR,
     PRIMARY KEY (id)
-)
+);
 --Reviews
-CREATE TABLE IF NOT EXISTS reviews (
-    id INT GENERATED ALWAYS AS IDENTITY
-    user_id INT REFERENCES users(id)
-    movie_id INT REFERENCES movies(id)
-    user_rating FLOAT
-    comments VARCHAR(500)
+CREATE TABLE reviews (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    user_id INT REFERENCES users(id),
+    movie_id INT REFERENCES movies(id),
+    user_rating FLOAT,
+    comments VARCHAR(500),
     PRIMARY KEY (id)
-)
+);
