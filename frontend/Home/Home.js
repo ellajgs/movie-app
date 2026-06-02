c2d5c8cc7efc59a7003ff6d8691ad22b261f70f5
“beata-dan” <beata.danilevskaya@gmail.com>
2026-06-02 15:42:51 +0100

    Updates home page

--- frontend/Home/Home.js
+++ frontend/Home/Home.js
@@ -1,36 +1,72 @@
 // const userName = localStorage.getItem("username");
 // document.getElementById("user-name").textContent = userName;
 
-document.getElementById("user-nav").addEventListener("click", () => {
-    window.location.assign()
+const tableBody = document.querySelector("#movie-table-body")
+
+document.getElementById("row-1").addEventListener("click", () => {
+    localStorage.setItem("movie-name", "The Dark Knight")
+    window.location.assign("../Movie/Movie.html")
 })
 
-document.getElementById("classes-nav").addEventListener("click", () => {
-    window.location.assign()
-})
+// const logoutBtn = document.querySelector()
 
-document.getElementById("scores-nav").addEventListener("click", () => {
-    window.location.assign()
-})
 
-document.getElementById("game-1").addEventListener("click", () => {
-    console.log("here");
-    localStorage.setItem("challengeId","1");
-    window.location.assign("../Game/Game.html");
-});
+// document.getElementById("user-nav").addEventListener("click", () => {
+//     window.location.assign()
+// })
 
-document.getElementById("game-2").addEventListener("click", () => {
-    alert("This game is under development!");
-});
+// document.getElementById("classes-nav").addEventListener("click", () => {
+//     window.location.assign()
+// })
 
-document.getElementById("game-3").addEventListener("click", () => {
-    alert("This game is under development!");
-});
+// document.getElementById("scores-nav").addEventListener("click", () => {
+//     window.location.assign()
+// })
 
-document.getElementById("game-4").addEventListener("click", () => {
-    alert("This game is under development!");
-});
+// document.getElementById("game-1").addEventListener("click", () => {
+//     console.log("here");
+//     localStorage.setItem("challengeId","1");
+//     window.location.assign("../Game/Game.html");
+// });
 
-document.getElementById("logout-nav").addEventListener("click", () => {
-    window.location.assign("../Login/login.html");
-});
+// document.getElementById("game-2").addEventListener("click", () => {
+//     alert("This game is under development!");
+// });
+
+// document.getElementById("game-3").addEventListener("click", () => {
+//     alert("This game is under development!");
+// });
+
+// document.getElementById("game-4").addEventListener("click", () => {
+//     alert("This game is under development!");
+// });
+
+// document.getElementById("logout-nav").addEventListener("click", () => {
+//     window.location.assign("../Login/login.html");
+// });
+
+
+function generateTableRow(){
+    
+        const data = [,
+            
+            {movie: "Dune",
+            review: 9},
+            {movie: "It",
+            review: 8}
+        ]
+        
+   
+     for(let i = 0; i < data.length; i++){
+        const tr = document.createElement("tr");
+        const dataObject = data[i]
+        for(let key in dataObject){
+            const td = document.createElement("td");
+            td.innerHTML = dataObject[key]
+            tr.appendChild(td)
+        }
+        tableBody.appendChild(tr)
+    }
+    
+}
+generateTableRow()
