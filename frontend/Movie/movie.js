1272aaa76c83a8cf0d764d0f2131463af89f164f
Sajanth Kirubaharan <sajanth02@gmail.com>
2026-06-02 12:24:27 +0100

    added movie table in html and js

---
+++ frontend/Movie/movie.js
@@ -0,0 +1,5 @@
+const movieHeader = document.querySelector("#movie-header")
+const movieTable = document.querySelector("#movie-table")
+const tableBody = document.querySelector("#movie-table-body")
+
+function getMovieInfo(){}
