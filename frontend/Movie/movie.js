30c47a772fc9e3a572690cef88bd6384119fac80
Sajanth Kirubaharan <sajanth02@gmail.com>
2026-06-02 12:50:02 +0100

    added back button

--- frontend/Movie/movie.js
+++ frontend/Movie/movie.js
@@ -1,5 +1,19 @@
 const movieHeader = document.querySelector("#movie-header")
 const movieTable = document.querySelector("#movie-table")
 const tableBody = document.querySelector("#movie-table-body")
+const backButton = document.querySelector("#back-button")
+
+backButton.addEventListener("click", () => {
+    window.location.assign("./Home/Home.html")
+})
 
 function getMovieInfo(){}
+
+const data;
+
+function generateTableRow(){
+    const tr = document.createElement("tr");
+    const td = document.createElement("td");
+    for(let i = 0; i++; i< data.length){}
+    
+}
