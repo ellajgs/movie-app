b8061227a6520745131397b6274e6189dd0d2880
ella <ejgsimmons@outlook.com>
2026-06-04 16:00:05 +0100

    add proper ratings

--- frontend/Movie/movie.js
+++ frontend/Movie/movie.js
@@ -121,8 +121,8 @@ async function displayMovieInfo(){
         <p><strong>Year:</strong> ${movie["movie_year"]}</p>
         <p><strong>Plot:</strong> ${movie["plot"] === null ? "N/A" : movie["plot"]}</p>
         <p><strong>IMDB Rating:</strong> ${movie["imdbrating"]}/10</p>
-        <p><strong>Rotten Tomatoes:</strong> ${movie["rtrating"]}/100</p>
-        <p><strong>Metacritic:</strong> ${movie["mcrating"]}/100</p>
+        <p><strong>Rotten Tomatoes:</strong> ${movie["rtrating"]}/10</p>
+        <p><strong>Metacritic:</strong> ${movie["mcrating"]}/10</p>
         <p><strong>Avg Site Rating:</strong> ${movie["avg_rating"]}/5</p>
 
     `
